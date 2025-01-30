8968a60ff1ef365173a94958ac6190c99ef9be3c
Ryan Palumbo <rynpal@gmail.com>
2025-01-30 11:15:02 -0500

    Simplify app.run configuration by removing development mode check

--- app.py
+++ app.py
@@ -94,8 +94,5 @@ def generate():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    if os.getenv('FLASK_ENV') == 'development':
-        app.run(host="0.0.0.0", port=port, debug=True)
-    else:
-        app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get('PORT', 5000))    
+    app.run(host="0.0.0.0", debug=False)
